4f9c86015cded28111a8cc3f18a4282df3a16ce9
Tomas Vazquez <vazquezt2018@gmail.com>
2023-02-25 12:50:17 -0300

    configurating sign in button component

---
+++ src/components/SignInbutton.tsx
@@ -0,0 +1,25 @@
+import { useAddress, useNetworkMismatch, useNetwork, ConnectWallet, ChainId} from '@thirdweb-dev/react'
+import React from 'react'
+
+type Props = {}
+
+export default function SignInbutton({}: Props) {
+  const address = useAddress(); // detect the connected address
+  const isOnWrongNetwork = useNetworkMismatch(); // detect if the user is on the wrong netfwork
+  const [, switchNetWork] = useNetwork(); // function to switch the network
+  // 1. User needts to connect their wallet
+  if (!address){
+    return <ConnectWallet />
+  }
+  // 2. Users need to switch network to polygon
+  if (isOnWrongNetwork){
+    return (
+      <button onClick={() => switchNetWork?.(ChainId.Polygon)}>
+        Switch Network
+      </button>)
+  }
+  // 3. Sign in with lens
+  // 4. Show the user profile on Lens
+
+
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import { QueryClientProvider, QueryClient} from '@tanstack/react-query'
+import { ThirdwebProvider, ChainId } from '@thirdweb-dev/react'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const desiredChainId = ChainId.Polygon;
   const queryClient = new QueryClient()
+  
 
-  return (<QueryClientProvider client={queryClient}>
-    <Component {...pageProps} />
-    </QueryClientProvider>)
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThirdwebProvider activeChain="ethereum">
+        <Component {...pageProps} />
+      </ThirdwebProvider>
+    </QueryClientProvider>
+    )
 }
